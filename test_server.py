a2e19a471c9d84f1532680e91c7c1430df3301ed
Muromati <enricoseo@gmail.com>
2017-10-01 18:49:25 +0300

    1

--- test_server.py
+++ test_server.py
@@ -4,8 +4,9 @@ import unittest
 
 class TestResponseFormat(unittest.TestCase):
     def test_response_format(self):
-        func = server.response_format('200', 'any_time', 'any_text')
-        self.assertEqual(func, '{"response": "100", "alert": "any_text", "time": "any_timestamp"}')
+        func = server.response_format('200', 'any_timestamp', 'any_text')
+        self.assertEqual(func, '{"response": "200", "time": "any_timestamp", "alert": "any_text"}')
 
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
