eb67a0ee594b9b50b1f2aed158f7acbd7b741054
Muromati <enricoseo@gmail.com>
2017-10-02 12:48:27 +0300

    Правки

--- test_client.py
+++ test_client.py
@@ -2,19 +2,11 @@ import client
 import unittest
 
 
-
 class TestPresenceFormat(unittest.TestCase):
-
     def test_presence_format(self):
         func = client.presence_format('presence', 'any_timestamp')
         self.assertEqual(func, '{"action": "presence", "time": "any_timestamp"}')
 
-    # def test_response_parse(self):
-    #     response = {
-    #         "response": "200",
-    #         "alert": "test_message"
-    #     }
-    #     self.assertEqual(client.response_parse(response), [str])
 
-
-unittest.TestCase()
+if __name__ == '__main__':
+    unittest.main()
--- test_server.py
+++ test_server.py
@@ -2,10 +2,11 @@ import server
 import unittest
 
 
+# Ничего не работает, тест не выполняется, голову сломал
 class TestResponseFormat(unittest.TestCase):
     def test_response_format(self):
         func = server.response_format('200', 'any_timestamp', 'any_text')
-        self.assertEqual(func, '{"response": "200", "time": "any_timestamp", "alert": "any_text"}')
+        self.assertEqual(func, '{"response": "100", "time": "any_timestamp", "alert": "any_text"}')
 
 
 if __name__ == '__main__':
