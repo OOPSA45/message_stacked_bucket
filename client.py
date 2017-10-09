1b45ec3467dd20c7fc6af8e14d6b30964cba40e2
Muromati <enricoseo@gmail.com>
2017-10-09 03:20:06 +0300

    Логирование

--- client.py
+++ client.py
@@ -1,9 +1,32 @@
 from socket import *
 import time
 import json
+import logging
+from functools import wraps
+
+import log_config
+
+LOG_LEVEL = logging.DEBUG
+log_config.set_logger('client', LOG_LEVEL)
+CLIENT_LOG = logging.getLogger('client')
+
+class Log:
+
+    def __call__(self, func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            result = func(*args, **kwargs)
+            CLIENT_LOG.debug('{}'.format(func.__name__))
+            return result
+        return inner
+
+
+log = Log()
+
 
 
 # Сформировать presence-сообщение в JIM
+@log
 def presence_format(action, time):
     message = {
         'action': action,
@@ -13,6 +36,7 @@ def presence_format(action, time):
 
 
 # Разобрать сообщение сервера
+@log
 def response_parse(response):
     output = 'Response code: ' + response['response'] + "\n"
     output += '--- ' + response['alert'] + ' ---'
@@ -20,6 +44,7 @@ def response_parse(response):
 
 
 # Отправляет сообщение серверу
+@log
 def send_message(presence_message, s):
     jpresence = json.dumps(presence_message)
     bpresence = jpresence.encode('utf-8')
@@ -27,6 +52,7 @@ def send_message(presence_message, s):
 
 
 # Получается сообщение от сервера
+@log
 def get_response(sock):
     bresponse = sock.recv(1024)
     jresponse = bresponse.decode('utf-8')
