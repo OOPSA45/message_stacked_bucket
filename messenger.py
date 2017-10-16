7631154e47b0338888fb70a50f54ed9827d72cd0
Muromati <enricoseo@gmail.com>
2017-10-16 23:21:26 +0300

    Базовые клиент + сервер на классах

--- messenger.py
+++ messenger.py
@@ -1,12 +1,9 @@
 # 1.	Класс JIMСообщение - класс, реализующий сообщение (msg) по протоколу JIM.
 # 2.	Класс JIMОтвет - класс, реализующий ответ (response) по протоколу JIM.
-
 # 3.	Класс Клиент - класс, реализующий клиентскую часть системы.
 
 # 4.	Класс Чат - класс, обеспечивающий взаимодействие двух клиентов.
-
 # 5.	Класс ЧатКонтроллер - класс, обеспечивающий передачу данных из Чата в ГрафическийЧат и обратно; обрабатывает события от пользователя (ввод данных, отправка сообщения).
-
 # 6.	Класс ГрафическийЧат - базовый класс, реализующий интерфейс пользователя (UI) - вывод сообщений чата, ввод данных от пользователя - служит базой для разных интерфейсов пользователя (консольный, графический, WEB).
 #           ■	Дочерний класс КонсольныйЧат - обеспечивает ввод/вывод в простой консоли.
 # 7.	Класс Сервер - базовый класс сервера мессенджера; может иметь разных потомков - работающих с потоками или выполняющих асинхронную обработку.
@@ -18,49 +15,30 @@ import json
 import time
 
 
-# MyMessMessage({'type': 'action', 'code': 'presence'}, time.time(), {'type': 'alert', 'message': 'test'})
 class MyMessMessage:
-    def __init__(self, cur_socket, action, cur_time, alert=None):
+    def __init__(self, cur_socket, raw_message=None):
         self.cur_socket = cur_socket
-        self.action_type = action['type']
-        self.action_code = action['code']
-        self.time = time.ctime(cur_time)
-        self.alert = alert
-
-        if self.alert is not None:
-            self.alert_type = alert['type']
-            self.alert_mess = alert['message']
-
-    def mess_format(self):
-        json_message = {
-            self.action_type: self.action_code,
-            'time': self.time,
-        }
-        if self.alert is not None:
-            json_message[self.alert_type] = self.alert_mess
-
-        json_message = json.dumps(json_message)
-        return json_message
+        self.raw_message = raw_message
 
     def mess_send(self):
         self.cur_socket.send(self.mess_format().encode('utf-8'))
         return self.mess_format().encode('utf-8')
 
-
-class MyMessResponse:
-    def __init__(self, cur_socket):
-        self.cur_socket = cur_socket
-
     @property
-    def resp_decode(self):
-        response = json.loads(self.cur_socket.recv(1024).decode('utf-8'))
-        return '{} {} {}'.format(response['response'], response['alert'], response['time'])
+    def mess_get(self):
+        mess = json.loads(self.cur_socket.recv(1024).decode('utf-8'))
+        return mess
+
+    def mess_format(self):
+        self.raw_message['time'] = time.time()
+        json_message = json.dumps(self.raw_message)
+        return json_message
 
 
 class MyMessClient:
     def __init__(self):
         self.client_socket = socket(AF_INET, SOCK_STREAM)
-        self.presence = MyMessMessage(self.client_socket, {'type': 'action', 'code': 'presence'}, time.time())
+        self.presence = MyMessMessage(self.client_socket, {'action': 'presence'})
 
     def client_connect(self):
         try:
@@ -70,13 +48,34 @@ class MyMessClient:
         else:
             self.presence.mess_send()
         finally:
-            response = MyMessResponse(self.client_socket)
-            print(response.resp_decode)
+            response = MyMessMessage(self.client_socket)
+            print(response.mess_get)
 
 
-client = MyMessClient()
-client.client_connect()
+class MyMessServer:
+    def __init__(self):
+        self.server_socket = socket(AF_INET, SOCK_STREAM)
+        self.server_socket.bind(('', 7777))
+        self.server_socket.listen(5)
+        print('Сервер запущен')
 
+    def server_accept_in(self):
+        client_socket, addr = self.server_socket.accept()
+        self.get_client_query(client_socket)
+
+    def get_client_query(self, client_socket):
+        get_message = MyMessMessage(client_socket).mess_get
+        self.server_mess_identify(get_message, client_socket)
+        return get_message
+
+    def server_mess_identify(self, mess, client_socket):
+        if mess['action'] == 'presence':
+            print('Получено от клиента {}'.format(mess))
+            self.server_send_response(client_socket)
+
+    def server_send_response(self, client_socket):
+        response = MyMessMessage(client_socket, {'response': '200', 'alert': 'Well done!'})
+        response.mess_send()
 
 # class MyMessChat:
 #     def __init__(self):
@@ -90,9 +89,7 @@ client.client_connect()
 #     def __init__(self):
 #     pass
 #
-# class MyMessServer:
-#     def __init__(self):
-#     pass
+
 #
 # class MyMessStorage:
 #     def __init__(self):
--- server.py
+++ server.py
@@ -1,52 +1,6 @@
-from socket import *
-import time
-import json
-import logging
-
-
-# Получает сообщение от клиента
-def get_presence(client):
-    bpresence = client.recv(1024)
-    jpresence = bpresence.decode('utf-8')
-    presence = json.loads(jpresence)
-    return presence
-
-
-# Отправляет сообщение клиенту
-def send_response(response, client):
-    jresponse = json.dumps(response)
-    bresponse = jresponse.encode('utf-8')
-    client.send(bresponse)
-
-
-# Форматирует в JIM
-def response_format(response_code, time, alert,):
-    response = {
-        "response": response_code,
-        "time": time,
-        "alert": alert
-    }
-    return response
-
-
-# Формирует ответ клиенту
-def presence_parse(presence_message):
-    if presence_message['action'] == 'presence':
-        response_message = response_format('200', time.ctime(time.time()), 'Presence well done!')
-    else:
-        response_message = response_format('400', time.ctime(time.time()), 'Unknown query')
-    return response_message
-
+from messenger import *
 
 if __name__ == '__main__':
-    print('Сервер запущен')
-    s = socket(AF_INET, SOCK_STREAM)    # Создал сокет TCP
-    s.bind(('', 7777))                  # Закрепил адрес
-    s.listen(5)                         # Ждёт входящий
-
+    server = MyMessServer()
     while True:
-        client, addr = s.accept()       # Принимает запрос на соединение
-        presence_message = get_presence(client)
-        print('Получено от клиента {}'.format(presence_message))
-        response = presence_parse(presence_message)
-        send_response(response, client)
+        server.server_accept_in()
