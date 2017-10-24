84a2371bcf656a70fbc4d404088c36820f945184
Muromati <enricoseo@gmail.com>
2017-10-24 21:10:07 +0300

    Коммент про значение CURRENT_TIMESTAMP в базе по дефолту

--- schema/my_schema.py
+++ schema/my_schema.py
@@ -3,8 +3,12 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, Text, MetaData
 from sqlalchemy import DateTime
-from sqlalchemy import func as sql_func
 from sqlalchemy.ext.declarative import declarative_base
+
+# Вот об этом я говорил на уроке, далее server_default=sql_func.now(),
+# позволяет сделать default - CURRENT_TIMESTAMP и не нужно будет отправлять значение из кода.
+# так правильно, или нужно лучше из кода отправлять datetime.now() ?
+from sqlalchemy import func as sql_func
 
 # ORM
 from sqlalchemy.orm import sessionmaker
@@ -21,7 +25,7 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     login = Column(String(64), nullable=False)
-    created_date = Column(DateTime, server_default=sql_func.now())
+    created_date = Column(DateTime, server_default=sql_func.now())  # <------
 
 
 class UserHistory(Base):
